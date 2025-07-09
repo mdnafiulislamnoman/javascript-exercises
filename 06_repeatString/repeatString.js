c0fffc6ff22764673eeba928a64dceaa42e8b149
mdnafiulislamnoman <software.engr.nafiul@gmail.com>
2025-07-09 21:08:01 +0600

    Solve the problem as described

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word,num) {
+    let string = "";
+    for (let index = 0; index <= num; index++) {
+        string = string + word;
+    }
+    return string;
 };
 
 // Do not edit below this line
