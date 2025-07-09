9784d9384b59e1a13fe66fb9494d488f4c9766f8
mdnafiulislamnoman <software.engr.nafiul@gmail.com>
2025-07-10 00:17:25 +0600

    Solved as told

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,num) {
+    const newArray = [];
+
+    arr.forEach((item) => {
+        if (!num.includes(item)){
+            newArray.push(item);
+        }
+    })
+    return newArray;
 };
 
 // Do not edit below this line
