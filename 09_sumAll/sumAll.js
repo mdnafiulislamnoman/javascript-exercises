2c6cdfde1c87a201ecc478b60c33dfda6d599cec
mdnafiulislamnoman <software.engr.nafiul@gmail.com>
2025-07-10 00:26:45 +0600

    Solved as told

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
 
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (min < 0 || max < 0) return "ERROR";
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+
+    sum = 0;
+
+    for (let i = a; i <= b; i++) {
+        sum+=i;
+        return sum;
+    }
 };
 
 // Do not edit below this line
